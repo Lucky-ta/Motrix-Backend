764b2154a9f593ec099143cf0f11956004b2ca23
Lukita <lucmaieski@gmail.com>
2022-08-26 17:12:55 -0300

    Implements error messages

---
+++ src/middlewares/errors/subjectErrors.ts
@@ -0,0 +1,6 @@
+const subjectErrors = {
+  nameError: { message: 'Name must have a value' },
+  descriptionError: { message: 'Description must have a value' },
+};
+
+export default subjectErrors;
--- src/middlewares/subjectsMidllewares.ts
+++ src/middlewares/subjectsMidllewares.ts
@@ -1,27 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
+import subjectErrors from './errors/subjectErrors';
 
 const nameValidate = (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.body;
+  if (!name) return res.status(404).json(subjectErrors.nameError);
+  if (name.length === 0) return res.status(404).json(subjectErrors.nameError);
 
-  if (!name) return res.status(404).json({ message: 'Nome nao deve ser nulo' });
-  if (name.length === 0) return res.status(404).json({ message: 'Nome nao deve ser nulo' });
   return next();
 };
 
 const descriptionValidate = (req: Request, res: Response, next: NextFunction) => {
   const { description } = req.body;
+  if (!description) return res.status(404).json(subjectErrors.descriptionError);
+  if (description.length === 0) return res.status(404).json(subjectErrors.descriptionError);
 
-  if (!description) {
-    return res
-      .status(404)
-      .json({ message: 'Descrição nao deve ser nulo' });
-  }
-
-  if (description.length === 0) {
-    return res
-      .status(404)
-      .json({ message: 'Descrição nao deve ser nulo' });
-  }
   return next();
 };
 
