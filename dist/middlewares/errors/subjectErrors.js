15aa06146d2246afb29cd3fc61e2fe4270ec7f68
Lukita <lucmaieski@gmail.com>
2022-08-26 18:00:03 -0300

    Switch port order

--- dist/middlewares/errors/subjectErrors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const subjectErrors = {
-    nameError: { message: 'Name must have a value' },
-    descriptionError: { message: 'Description must have a value' },
-};
-exports.default = subjectErrors;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Runing on port ${PORT}`);
